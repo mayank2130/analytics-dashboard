b6c16b5c633c924a2964235068bb484097dd6718
mayank2130 <mayankthakur1712@gmail.com>
2024-09-27 23:01:38 +0530

    date entry for visits

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,9 +1,13 @@
 import { redis } from "@/lib/redis";
+import { getDate } from ".";
 
 type AnalyticsArgs = {
   retention?: number;
 };
 
+type TrackOptions = {
+  persist?: boolean;
+};
 export class Analytics {
   private retention: number = 60 * 60 * 24 * 7;
 
@@ -11,10 +15,15 @@ export class Analytics {
     if (opts?.retention) this.retention = opts.retention;
   }
 
-  async track(namespace: string, event: object = {}) {
-    const key = `analytics::${namespace}`;
+  async track(namespace: string, event: object = {}, opts?: TrackOptions) {
+    let key = `analytics::${namespace}`;
+
+    if (!opts?.persist) {
+      key += `::${getDate()}`;
+    }
 
     await redis.hincrby(key, JSON.stringify(event), 1);
+    if (!opts?.persist) await redis.expire(key, this.retention);
   }
 }
 
