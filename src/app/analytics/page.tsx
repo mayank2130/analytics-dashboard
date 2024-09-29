832e16eeda6b210ead6153012fb1c918d3fd35e1
mayank2130 <mayankthakur1712@gmail.com>
2024-09-29 17:46:46 +0530

    avg visitors and total visitors a day reduce logic

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,13 +1,45 @@
+import AnalyticsDashboard from "@/components/AnalyticsDashboard";
+import { getDate } from "@/utils";
 import { analytics } from "@/utils/analytics";
 import React from "react";
 
 type Props = {};
 
 const Page = async (props: Props) => {
+  const TRACKING_DAYS = 7;
 
-  const pageview = await analytics.retrieve("pageview", "28/09/2024");
+  const pageviews = await analytics.retrieveDays("pageview", 7);
 
-  return <p>{JSON.stringify(pageview)}</p>;
+  const totalPageviews = pageviews.reduce((acc, curr) => {
+    return (
+      acc +
+      curr.events.reduce((acc, curr) => {
+        return acc + Object.values(curr)[0]!;
+      }, 0)
+    );
+  }, 0);
+
+  const avgVisitorsPerDay = (totalPageviews / TRACKING_DAYS).toFixed(1);
+
+  const amtVisitorsToday = pageviews
+    .filter((ev) => ev.date === getDate())
+    .reduce((acc, curr) => {
+      return (
+        acc +
+        curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0)
+      );
+    }, 0);
+
+  return (
+    <div className="min-h-screen w-full py-12 flex justify-center items-center">
+      <div className="relative w-full max-w-6xl mx-auto text-white">
+        <AnalyticsDashboard
+          avgVisitorsPerDay={avgVisitorsPerDay}
+          amtVisitorsToday={amtVisitorsToday}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
